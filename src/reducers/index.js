03b6b9420182aed2315e1d79bf356130a16d136e
Phani17 <phanindrasarma20@gmail.com>
2017-08-21 09:51:38 +0530

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,7 +3,6 @@ import BooksReducer from './books_reducer'
 import ActiveBook from './active_book_reducer'
 
 const rootReducer = combineReducers({
-  //state: (state = {}) => state
   activeBook:ActiveBook,
   books:BooksReducer
 });
